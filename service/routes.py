13241c1e20cab62b50264a21f26194d146b773a2
adamcati <adamcati@yahoo.com>
2022-11-15 15:00:37 -0500

    Implement posibility to update an account

--- service/routes.py
+++ service/routes.py
@@ -84,8 +84,17 @@ def read_account(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
-
-
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND,f"Account with id [{account_id}] could not be found.")
+    
+    account.deserialize(request.get_json())
+    account.update()
+    return make_response(
+        jsonify(account.serialize()), status.HTTP_200_OK
+    )
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,3 +138,17 @@ class TestAccountService(TestCase):
             f"{BASE_URL}/0", content_type="application/json"
         )
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_account(self):
+        test_account = AccountFactory()
+        response = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        new_account = response.get_json()
+        new_account["name"] = "Updated name"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], new_account["name"])
+
+
+
